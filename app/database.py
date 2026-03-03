240826e1ebcd7f61c129d324469e063321bfa4c7
muhammads-git <roadprogrammar@gmail.com>
2026-03-03 15:37:54 +0500

    building phase

--- app/database.py
+++ app/database.py
@@ -1,1 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
+
+# load . env
+load_dotenv()
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+
+# create engine
+engine = create_engine(DATABASE_URL)
+
+
 url_db = {}
